f3cec623bf7b17fe14e4cd311d32bcb29c69956e
IamLizu <smmh.lzui@gmail.com>
2021-04-10 01:43:30 +0600

    setup & config tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="">
+            <header className="">
+                <h1 className="text-4xl">Hello World</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
