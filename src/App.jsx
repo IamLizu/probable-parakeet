11c9164d6927f0c5548e1a8d620947f870ac77a5
IamLizu <smmh.lzui@gmail.com>
2021-04-10 02:26:14 +0600

    identity, title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { user as pUser } from "./data/about";
+
 function App() {
     return (
         <div className="">
-            <header className="">
-                <h1 className="text-4xl">Hello World</h1>
+            <header className="px-10 pt-6">
+                <h1 className="text-gray-900">
+                    <span className="text-4xl">
+                        <span className="text-gray-600 font-bold hover:text-indigo-800">
+                            <a href={pUser.website}>{pUser.name}</a>
+                        </span>
+                        ,
+                    </span>
+                    <br />
+                    <span className="text-2xl text-gray-500 cursor-default">
+                        {pUser.title}
+                    </span>
+                </h1>
             </header>
         </div>
     );
---
+++ src/data/about.js
@@ -0,0 +1,5 @@
+export const user = {
+    name: "S. M. Mahmudul Hasan",
+    website: "https://smmahmudulhasan.com",
+    title: "Backend Developer based in Bangladesh.",
+};
